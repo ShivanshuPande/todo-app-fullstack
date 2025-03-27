b0cb5cf0e603b2995cf46284220d53055dd99d8a
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-03-27 20:44:33 +0530

    deleted my old version just trying to do it again from different method -- i dont whether i should be doing it or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.  svg'
 import './App.css'
-import { CreateTodo } from './components/createTodo'
-import { Todo } from './components/Todo'
 
 function App() {
-  // const [todos , setTodo] = useState([])
-  // fetch("http://localhost:3000/todos")
-  // .then(async function (res) {
-  //     const json = await res.json();
-  //     setTodo(json.todos);
-  // })
-
-  const [todos,setTodo] = useState ([])
-  useEffect(()=>{
-    fetch("http://localhost:3000/todos")
-    .then(async function (res){
-      const json = await res.json();
-      setTodo(json.todos)// g
-    } ,[todos])//run when the todos as a state variable changes
-  })
   
   return (
-    <div>
-        <CreateTodo></CreateTodo>
-        <Todo todos ={todos}></Todo>
+    <div className='bg-amber-600'>
+        Hi there
     </div>
   )
 }
